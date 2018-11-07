bea75355a0696efb40f3727d6354ca8d7a35b7dc
ramce <ramce@10.30.4.119>
2018-11-06 20:41:50 -0600

    Hola

--- src/main/java/unitec/primavera/ControladorMensajitos.java
+++ src/main/java/unitec/primavera/ControladorMensajitos.java
@@ -8,6 +8,7 @@ package unitec.primavera;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = {"https://angular-ghiryr.stackblitz.io"})
 public class ControladorMensajitos {
     @Autowired RepositorioMensajes mensa;
     
